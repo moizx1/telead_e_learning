a94c3f73c58d08eabdbea8def4dae52e100e3e2a
Abdul Muiz <abdulmuizpk@gmail.com>
2023-07-24 18:06:17 +0500

    added registration page

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:telead_e_learning/widget/custom_text_field.dart';
 
 class Register extends StatelessWidget {
   const Register({super.key});
@@ -6,10 +7,17 @@ class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffF5F9FF),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [Image.asset('assets/images/TeleadLogo.png')],
+          children: [
+            Image.asset('assets/images/TeleadLogo.png'),
+            CustomTextField(
+                hintText: 'Password',
+                prefixIcon: Icons.lock_outline_rounded,
+                suffixIcon: Icons.visibility_off_outlined),
+          ],
         ),
       ),
     );
--- lib/widget/custom_text_field.dart
+++ lib/widget/custom_text_field.dart
@@ -15,47 +15,42 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      body: Center(
-        child: Container(
-          height: 60,
-          decoration: BoxDecoration(
-            color: const Color(0xffFFFFFF),
-            borderRadius: BorderRadius.circular(12.0),
-          ),
-          child: Center(
-            child: TextFormField(
-              obscureText: isTextVisible,
-              textAlignVertical: TextAlignVertical.center,
-              decoration: InputDecoration(
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.only(left: 7.5),
-                  child: Icon(
-                    prefixIcon,
-                    size: 25,
-                  ),
-                ),
-                suffixIcon: Padding(
-                  padding: const EdgeInsets.only(right: 7.5),
-                  child: IconButton(
-                    onPressed: onPress,
-                    icon: Icon(
-                      suffixIcon,
-                      size: 25,
-                    ),
-                  ),
-                ),
-                border: InputBorder.none,
-                hintText: hintText,
-                hintStyle: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff505050),
-                ),
-                contentPadding: const EdgeInsets.only(right: 10),
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        color: const Color(0xffFFFFFF),
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Center(
+        child: TextFormField(
+          obscureText: isTextVisible,
+          textAlignVertical: TextAlignVertical.center,
+          decoration: InputDecoration(
+            prefixIcon: Padding(
+              padding: const EdgeInsets.only(left: 7.5),
+              child: Icon(
+                prefixIcon,
+                size: 25,
               ),
             ),
+            suffixIcon: Padding(
+              padding: const EdgeInsets.only(right: 7.5),
+              child: IconButton(
+                onPressed: onPress,
+                icon: Icon(
+                  suffixIcon,
+                  size: 25,
+                ),
+              ),
+            ),
+            border: InputBorder.none,
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Color(0xff505050),
+            ),
+            contentPadding: const EdgeInsets.only(right: 10),
           ),
         ),
       ),
