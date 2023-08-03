18372a92acfa6da7725b82af2c8e001de7417113
Abdul Muiz <abdulmuizpk@gmail.com>
2023-08-03 14:57:05 +0500

    .

--- lib/widget/mentor_card.dart
+++ lib/widget/mentor_card.dart
@@ -1,50 +1,62 @@
 import 'package:flutter/material.dart';
 
 class MentorCard extends StatelessWidget {
-  const MentorCard({super.key, this.name, this.category, this.imageUrl});
+  const MentorCard(
+      {super.key,
+      this.name,
+      this.category,
+      this.imageUrl,
+      this.showMessageIcon = false});
   final String? name, category, imageUrl;
+  final bool? showMessageIcon;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              height: 66,
-              width: 66,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(width: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            Row(
               children: [
-                Text(
-                  name ?? '',
-                  style: const TextStyle(
-                    color: Color(0xff202244),
-                    fontSize: 17,
-                    fontWeight: FontWeight.w600,
+                Container(
+                  height: 66,
+                  width: 66,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
                   ),
                 ),
-                Text(
-                  category ?? '',
-                  style: const TextStyle(
-                    color: Color(0xff545454),
-                    fontSize: 13,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name ?? '',
+                      style: const TextStyle(
+                        color: Color(0xff202244),
+                        fontSize: 17,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      category ?? '',
+                      style: const TextStyle(
+                        color: Color(0xff545454),
+                        fontSize: 13,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
               ],
-            )
+            ),
+            if (showMessageIcon == true) Icon(Icons.message_outlined),
           ],
         ),
-        const SizedBox(height: 20),
-        const Divider(color: Color.fromARGB(255, 212, 223, 242)),
-        const SizedBox(height: 20),
+        const SizedBox(height: 15),
+        const Divider(color: Color.fromARGB(255, 201, 213, 234), thickness: 1),
+        const SizedBox(height: 15),
       ],
     );
   }
---
+++ lib/widget/review_card.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class ReviewCard extends StatelessWidget {
+  const ReviewCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 46,
+          width: 46,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.black,
+          ),
+        ),
+        SizedBox(
+          width: 12,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'William S. Cunningham',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff202244),
+                    ),
+                  ),
+                  Container(
+                    // alignment: Alignment.center,
+                    height: 26,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Color(0xffE8F1FF),
+                      borderRadius: BorderRadius.circular(13),
+                      border: Border.all(color: Color(0xff4D81E5), width: 2.5),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.star_rounded,
+                          size: 15,
+                          color: Color(0xffFF9C07),
+                        ),
+                        Text(
+                          '4.5',
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xff202244),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Text(
+                  'The Course is Very Good dolor sit amet, consect tur adipiscing elit. Naturales divitias dixit parab les esse, quod parvo'),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Row(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.favorite,
+                        size: 15,
+                        color: Colors.red,
+                      ),
+                      SizedBox(width: 7.5),
+                      Text(
+                        '578',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff202244),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 30),
+                  Text(
+                    '2 Weeks Ago',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff202244),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
